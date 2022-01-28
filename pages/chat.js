b31841f2d07ed4c8931a0f50deec35737bda29ad
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-01-28 19:53:35 -0300

    Aula 04

--- pages/chat.js
+++ pages/chat.js
@@ -27,7 +27,7 @@ export default function ChatPage() {
   function handleNovaMensagem(novaMensagem) {
     const mensagem = {
       // id: listaDeMensagens.length + 1,
-      de: 'vanessametonini',
+      de: 'MatheusSimoes13',
       texto: novaMensagem,
     };
 
